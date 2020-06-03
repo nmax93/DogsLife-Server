3647420024f5846c3095716caa35d724ad7b3f27
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-06-03 14:28:36 +0300

    user schema update

--- Schemas/UserSchema.js
+++ Schemas/UserSchema.js
@@ -1,42 +1,39 @@
 const mongoose = require('mongoose')
-var Schema = mongoose.Schema;
 
-const visitedGardenSchema = Schema({
-  garden_id: Number,
-  last_visit: { type: Date, default: Date.now },
-  total_visits: { type: Number, default: 0 },
-});
 const schema = {
   id: Number,
-  private: Boolean,
   device_mac_id: String,
   name: String,
-  age: Number,
-  gender: String,
+  age: Date, //or string
+  gender: Boolean, //false m, true fm
   avatar: String,
   dogs: [Number],
-  preferences: {
-    dogs: {
-      breads: [String],
-      min_weight: Number,
-      max_weight: Number
-    },
-    owners: {
-      gender: String,
-      min_age: Number,
-      max_age: Number
-    }
-  },
-  owner_matches: [Number],
-  dog_matches: [Number],
-  visited_gardens: [visitedGardenSchema]
+  matches: [ {
+    owner: Number,
+    my_dog: Number,
+    with_dog: Number,
+    collar_match: { type: Boolean, default: false }
+  } ],
+  address: {
+    lat: String,
+    lng: String,
+    radiusInMeters: Number
+  }, 
+
+  hobbies: [Boolean], //[size 8]           ///m 32%
+  walk_routine: Object,                          ///m 20%  if same type +10, if duration +10 ---deviation 15 min
+  hangouts: [Boolean],    //[size 4]       ///m 28%
+  number_of_dogs: Number, //if equal       ///m 10%
+  raise_with: Number,     //if same (3 options)  ///m 10%
+  feeding_hours: {
+    morning: Number,
+    noon: Number,
+    evening: Number
+  }
 }
 
-
 const users_schema = new mongoose.Schema(schema)
-const user = mongoose.model('user', users_schema)
-const visitedGarden = mongoose.model('visitedGarden', visitedGardenSchema)
+const User = mongoose.model('user', users_schema)
 module.exports = {
-  user,
-  visitedGarden
+  User
 } 
