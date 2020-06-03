b633a9b07d865abf2f3c505a71225784a5bc1deb
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-06-03 14:29:36 +0300

    dog schema update

--- Schemas/DogSchema.js
+++ Schemas/DogSchema.js
@@ -1,18 +1,55 @@
 const mongoose = require('mongoose')
+var Schema = mongoose.Schema;
 
-const schema = {
+// const visitedGarden = {
+//   garden_id: Number,
+//   last_visit: { type: Date, default: Date.now },
+//   total_visits: { type: Number, default: 0 },
+//   avg_play_time: Number, // in minutes
+// }
+const visitedGardenSchema = Schema({
+   garden_id: Number,
+  last_visit: { type: Date, default: Date.now },
+  total_visits: { type: Number, default: 0 },
+      avg_play_time: { type: Number, default: 0 }, // in minutes
+ });
+
+const schema = { //60%
   id: Number,
   collar_mac_id: String,
-  service_uuid: String,
-  owner: Number,
+  service_uuid: String, //?
+  owners: [Number],
   name: String,
-  age: Number,
-  bread: String,
-  weight: Number,
-  other_owners: [Number],
-  avatar: String
+  description: String,
+  avatar: String,
+  visited_gardens: [visitedGardenSchema],
+  get_along: {                               ///m c 100%
+    gender: Number,  //1 m, 2 fm, 3 both
+    spayed: Number, //1 spayed, 2 not spayed, 3 both
+    size: Number,    //1 small, 2 big, 3 both
+  },
+  physical_params: {
+    breed: String,                           ///m 10%
+    mixed: Boolean,
+    gender: Number,
+    age: Date, //or string
+    weight: Number,
+    spayed: Number, //1 apayed, 2 not spayed
+    size: Number, //1 small, 2 big
+    howMuchExercise: Number
+    
+  },
+  character_params: {
+    energy_level: Number,     //size 1-3      ///m deviation of 1   60%
+    avg_play_time: Number,
+    playfullness: Boolean,                   ///m                  60%
+    park_preference: Number    //1 empty, 2 crowded, 3 both
+  },
 }
 const dogs_schema = new mongoose.Schema(schema)
 const dog = mongoose.model('dog', dogs_schema)
+const visitedGarden = mongoose.model('visitedGarden', visitedGardenSchema)
 
-module.exports = dog
+module.exports =  {
+  dog,
+  visitedGarden }
