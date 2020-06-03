2608247a1eddf125b85e5fd94df391e3fabc326e
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-06-03 14:40:25 +0300

    new functions

--- index.js
+++ index.js
@@ -18,18 +18,24 @@ app.post('/version', (req, res, next) => {
 app.post('/register', accountController.register)
 app.post('/login', accountController.login)
 //PROFILE
-app.get('/getUsers', profileController.getUsers)
+app.post('/getUserProfile', profileController.getUserProfile)
 app.post('/editUserProfile', profileController.editUserProfile)
 app.post('/editDogProfile', profileController.editDogProfile)
 app.post('/addDog', profileController.addDog)
+app.post('/addSignupObject', profileController.addSignupObject)
+app.post('/addNewUserToExistDog', profileController.addNewUserToExistDog)
+
 //MATCH
-app.post('/createDogMatch', matchController.createDogMatch)
+app.post('/createDogMatch', urlencodedParser, matchController.createDogMatch)
+
 app.post('/getMatches', matchController.getMatches)
 //MAP
 app.get('/getGardens', mapController.getGardens)
 app.post('/getPresentDogsInGarden', mapController.getPresentDogsInGarden)
 app.post('/dogsEnterGarden', urlencodedParser, mapController.dogsEnterGarden)
-// app.post('/dogExitGarden', mapController.dogExitGarden)
+app.post('/addReview', mapController.addReview)
+app.post('/getDogOwners', mapController.getDogOwners)
+app.post('/getOwnerDogs', mapController.getOwnerDogs)
 
 app.listen(5050)
 console.log('Server is running')
