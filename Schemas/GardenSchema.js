5b18d089763faeb4b32e497631f36f789009354b
henaharon@se.shenkar.ac.il <henaharon@se.shenkar.ac.il>
2020-06-03 14:29:00 +0300

    garden schema update

--- Schemas/GardenSchema.js
+++ Schemas/GardenSchema.js
@@ -3,6 +3,7 @@ var Schema = mongoose.Schema;
 
 const dogVisitorSchema = Schema({
   dog_id: Number,
+  first_scan: { type: Date, default: Date.now }, // when dog is presented in garden we calculate how many minutes left in avg
   last_scan: { type: Date, default: Date.now },
   total_attendance_minutes: { type: Number, default: 0 },
 });
@@ -19,11 +20,21 @@ const gardenSchema = Schema({
   lat: Number,
   long: Number,
   present_dogs: [Number],
-  daily_visitors: [dayVisitorsSchema]
+  daily_visitors: [dayVisitorsSchema],
+  rating: {
+    cleanliness_score: Number,
+    facilities_score: Number,
+    user_reviews: [
+      {
+        author: String,
+        user_id: Number,
+        date: { type: Date, default: Date.now },
+        text: String
+      }
+    ]
+  }
 });
 
-
-// const Visitor = mongoose.model('Visitor', visitorSchema);
 const Garden = mongoose.model("Garden", gardenSchema);
 const DogVisitor = mongoose.model("DogVisitor", dogVisitorSchema);
 
