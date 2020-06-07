835ea8ccfc32fe2312586f8b535bd05cd4b045a2
Max <nmax1993@gmail.com>
2020-06-07 17:39:48 +0300

    avg time per garden

--- Schemas/DogSchema.js
+++ Schemas/DogSchema.js
@@ -8,10 +8,7 @@ const schema = { //60%
   name: String,
   description: String,
   avatar: String,
-  avg_time_in_garden: {
-    time: Number,
-    visits: Number
-  },
+  visited_gardens: [Object],
   get_along: {                               ///m c 100%
     gender: Number,  //1 m, 2 fm, 3 both
     spayed: Number, //1 spayed, 2 not spayed, 3 both
